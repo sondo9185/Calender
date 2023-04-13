b880b57e9cbddda45e73c9f03dece8a415f66ebe
sondongwoo <sondo9185@naver.com>
2023-04-14 01:05:14 +0900

    탭바 추가

---
+++ lib/tapbar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TapBar extends StatefulWidget {
+  const TapBar({super.key});
+
+  @override
+  State<TapBar> createState() => _TapBarState();
+}
+
+class _TapBarState extends State<TapBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

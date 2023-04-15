b1c74d6726af0bf5916d50b833ed6d1ca3ada3b9
sondongwoo <sondo9185@naver.com>
2023-04-15 16:37:57 +0900

    탭바에 3개의 탭 생성

--- lib/tapbar.dart
+++ lib/tapbar.dart
@@ -1,16 +1,107 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class TapBar extends StatefulWidget {
-  const TapBar({super.key});
+  const TapBar({Key? key}) : super(key: key);
 
   @override
   State<TapBar> createState() => _TapBarState();
 }
 
-class _TapBarState extends State<TapBar> {
+class _TapBarState extends State<TapBar> with TickerProviderStateMixin{
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(
+      length: 3, vsync: this,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar : AppBar(
+          title: const Text(
+            '일정 관리'
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: TabBar(
+                tabs: [
+                  Container(
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '날씨',
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '달력',
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '일정 목록',
+                    ),
+                  ),
+                ],
+                labelColor: Colors.red,
+                unselectedLabelColor: Colors.black,
+                controller: _tabController,
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '날씨 화면',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '달력 화면',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '일정 관리 화면',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
